854902f22bbb48b5ee2963cffb2e76fc138dbc22
abrilboya-code <abril.boya@estudiantat.upc.edu>
2026-04-28 08:47:32 +0200

    Update projecte.launch.py

--- launch/projecte.launch.py
+++ launch/projecte.launch.py
@@ -3,22 +3,22 @@ from launch_ros.actions import Node
 def generate_launch_description(): 
   return LaunchDescription([ 
     Node( 
-      package='treball_final_pkg', 
+      package='projecte_final_pkg', 
       executable='deteccio_exe', # El nom que posarem al setup.py 
       name='deteccio' 
     ), 
     Node( 
-      package='treball_final_pkg', 
+      package='projecte_final_pkg', 
       executable='moviment_exe', # El nom que posarem al setup.py 
       name='moviment' 
     ), 
     Node( 
-      package='treball_final_pkg', 
+      package='projecte_final_pkg', 
       executable='cartograf_exe', # El nom que posarem al setup.py 
       name='cartograf' 
     ), 
     Node( 
-      package='treball_final_pkg', 
+      package='projecte_final_pkg', 
       executable='safety_node_exe', # El nom que posarem al setup.py 
       name='safety' 
     ), 
