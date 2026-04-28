2cd23245f14be78baa4ebe2a04156fbdb29fcc12
abrilboya-code <abril.boya@estudiantat.upc.edu>
2026-04-28 08:38:06 +0200

    setup.py

---
+++ launch/setup.py
@@ -0,0 +1,31 @@
+import os 
+from glob import glob 
+from setuptools import setup 
+package_name = 'projecte_final_pkg' 
+setup( 
+  name=package_name, 
+  version='0.0.0', 
+  packages=[package_name], 
+  data_files=[ 
+    ('share/ament_index/resource_index/packages', 
+    ['resource/' + package_name]), 
+    ('share/' + package_name, ['package.xml']), 
+    # Aquesta línia instal·la els Launch files (es nova) 
+    (os.path.join('share', package_name, 'launch'), 
+glob(os.path.join('launch', '*launch.[pxy][yma]*'))), 
+  ], 
+  install_requires=['setuptools'], 
+  zip_safe=True, 
+  maintainer='tu', 
+  description='projecte final', 
+  license='Apache License 2.0', 
+  tests_require=['pytest'], 
+  entry_points={ 
+    'console_scripts': [ 
+      'safety_node_exe = projecte_final_pkg.safety_node:main', 
+      'cartograf_exe = projecte_final_pkg.cartograf:main', 
+      'moviment_exe = projecte_final_pkg.moviment:main', 
+      'deteccio_exe = projecte_final_pkg.deteccio:main', 
+    ], 
+  }, 
+)
