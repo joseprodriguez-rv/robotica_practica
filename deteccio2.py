7a8afe0de3e0ac255e9e5d5f5a78119f8a53a29f
abrilboya-code <abril.boya@estudiantat.upc.edu>
2026-04-29 17:17:05 +0200

    detecció sense logica de maniobra

---
+++ deteccio2.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from std_msgs.msg import String, Bool, Int32
+import math
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
+
+class DeteccioNode(Node):
+    def __init__(self):
+        super().__init__('deteccio')
+
+        #guardar on està el robot
+        self.robot_x = 0.0
+        self.robot_y = 0.0
+        self.robot_ang = 0.0
+
+        # qos per al sensor
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10
+        )
+
+        #subs al laser
+        self.sub_laser = self.create_subscription(
+            LaserScan, '/scan', self.laser_callback, qos_profile)
+
+        #subs a l'odom (P5)
+        self.sub_odom = self.create_subscription(
+            Odometry, '/odom', self.odom_callback, 10)
+
+#he tret la subs a maniobra
+
+        #subscripció al comptador d'objectes
+        self.objectes = 0
+        self.sub_comptador = self.create_subscription(
+            Int32, '/comptador_objectes', self.comptador_callback, 10)
+
+        #publisher objecte en odometria amb x i y
+        self.pub_objecte = self.create_publisher(Odometry, '/objecte_detectat', 10)
+        self.pub_tipus = self.create_publisher(String, '/tipus_obstacle', 10)
+
+        self.get_logger().info('Node de Detecció actiu')
+#he tret qos del moviment perquè aquí no moc el robot
+
+    def odom_callback(self, msg):
+        #posició actual
+        self.robot_x = msg.pose.pose.position.x
+        self.robot_y = msg.pose.pose.position.y
+
+        #conversió a Yaw
+        qx = msg.pose.pose.orientation.x
+        qy = msg.pose.pose.orientation.y
+        qz = msg.pose.pose.orientation.z
+        qw = msg.pose.pose.orientation.w
+        siny_cosp = 2 * (qw * qz + qx * qy)
+        cosy_cosp = 1 - 2 * (qy * qy + qz * qz)
+        self.robot_ang = math.atan2(siny_cosp, cosy_cosp)
+
+ #he tret la funció maniobra callback
+
+    def comptador_callback(self, msg):
+        self.objectes = msg.data
+
+    def laser_callback(self, msg):
+        # si està en maniobra, sí que ha de detectar per poder-se aturar
+        if self.objectes >= 5:
+            return
+
+        #con frontal
+        part_esquerra = msg.ranges[0:61]
+        part_dreta = msg.ranges[300:360]
+        con_frontal = list(part_esquerra) + list(part_dreta)
+
+        #treure valors invàlids
+        distancies_valides = [d for d in con_frontal if msg.range_min < d < msg.range_max]
+
+        if len(distancies_valides) > 0:
+            distancia_min = min(distancies_valides)
+
+            #si detectem un obstacle a prop
+            if distancia_min < 0.25:
+                #si és mur o objecte
+                tipus = String()
+                if len(distancies_valides) > 90:
+                    tipus.data = 'PARET'
+                    self.get_logger().info('PARET detectada')
+                else:
+                    #buscar l'angle
+                    tipus.data = 'OBJECTE'
+                    num_min = msg.ranges.index(distancia_min) #per trobar l'angle on està l'objecte
+                    angle = msg.angle_min + (num_min * msg.angle_increment)
+                    #ros2 topic echo /scan --once per comprobar si això existeix
+                    self.get_logger().warn(f'Objecte detectat a {distancia_min:.2f}m')
+                    self.enviar_posicio_objecte(distancia_min, angle)
+                self.pub_tipus.publish(tipus)
+
+    def enviar_posicio_objecte(self, r, a):
+        #suma d'angle del robot + angle del laser
+        angle_final = self.robot_ang + a
+
+        obj_x = self.robot_x + (r * math.cos(angle_final))
+        obj_y = self.robot_y + (r * math.sin(angle_final))
+
+        msg_obj = Odometry()
+        msg_obj.header.stamp = self.get_clock().now().to_msg()
+        msg_obj.header.frame_id = 'map'
+        msg_obj.pose.pose.position.x = float(obj_x)
+        msg_obj.pose.pose.position.y = float(obj_y)
+
+        self.pub_objecte.publish(msg_obj)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DeteccioNode()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Aturant node...')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+#es llegeix la posició amb msg.pose.pose.position.x
